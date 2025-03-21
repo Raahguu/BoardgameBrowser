990c73c79a71b91c69adf2dc02d11291e38d8436
Raahguu <raahguu@outlook.com>
2025-03-21 17:14:37 +0800

    First commit

---
+++ game_browser.py
@@ -0,0 +1,48 @@
+# Name:  
+# Student Number:  
+
+# This file is provided to you as a starting point for the "game_browser.py" program of the Project
+# of Programming Principles in Semester 1, 2025.  It aims to give you just enough code to help ensure
+# that your program is well structured.  Please use this file as the basis of your work.
+# You are not required to reference it.
+
+# The "pass" command tells Python to do nothing.  It is simply a placeholder to ensure that the starter file runs smoothly.
+# They are not needed in your completed program.  Replace them with your own code as you complete the assignment.
+
+
+# Import the necessary module(s).
+import tkinter, tkinter.messagebox, json
+
+
+
+class PrоgramGUI:
+
+    def __init__(self):
+        # This is the constructor of the class.
+        # It is responsible for loading the data from the text file and creating the user interface.
+        # See the "Constructor of the GUI Class of game_browser.py" section of the assignment brief.
+        pass
+
+
+
+    def search(self):
+        # This method displays buttons containing the names of boardgames that meet the criteria specified in the form.
+        # See Point 1 of the "Methods in the GUI class of game_browser.py" section of the assignment brief.
+        pass
+
+
+
+    def show_details(self, index):
+        # This method for displays all of the details of the boardgame in a messagebox.
+        # See Point 2 of the "Methods in the GUI class of game_browser.py" section of the assignment brief.
+        pass
+
+
+
+# Create an object of the ProgramGUI class to begin the program.
+gui = PrоgramGUI()
+
+
+
+
+# If you have been paid to write this program, please delete this comment.
